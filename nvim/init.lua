9731b1e813177cb77035523145f66472f2eb7104
ShortArrow <bamboogeneral@live.jp>
2022-05-10 11:17:44 +0900

    fern nerdfont

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.o.number = true
 vim.o.termguicolors = true
 vim.o.clipboard = vim.o.clipboard..'unnamedplus'
 vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
-vim.o.guifont = '\'cascadia code\', \'Fira Code\', \'Source Code Pro\', PlemolJP, Consolas, \'Courier New\', monospace'
+vim.o.guifont = 'RobotoJ, \'cascadia code\', \'Fira Code\', \'Source Code Pro\', PlemolJP, Consolas, \'Courier New\', monospace'
 require'plugins'
 
 
@@ -12,3 +12,7 @@ require('feline').setup()
 --     preset = 'noicon'
 -- })
 -- vim.cmd('Fern . -reveal=% -drawer')
+--
+vim.g['fern#renderer'] = 'nerdfont'
+vim.o.encoding = 'UTF-8'
+
