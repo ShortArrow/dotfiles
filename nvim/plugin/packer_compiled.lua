61337dcc305af8c3fec86f12fd352c829ed62537
shortarrow <bamboogeneral@shortarrow.jp>
2022-05-19 17:19:17 +0900

    fic

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -69,6 +69,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["FixCursorHold.nvim"] = {
+    loaded = true,
+    path = "/home/verde/.local/share/nvim/site/pack/packer/start/FixCursorHold.nvim",
+    url = "https://github.com/antoinemadec/FixCursorHold.nvim"
+  },
   ["feline.nvim"] = {
     loaded = true,
     path = "/home/verde/.local/share/nvim/site/pack/packer/start/feline.nvim",
