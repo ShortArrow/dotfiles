c8543ce80f808df206c36beb43f986d23e071d00
shortarrow <bamboogeneral@shortarrow.jp>
2022-05-27 12:29:06 +0900

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,35 +10,12 @@ require'plugins'
 -- # UI
 -- #############################
 require('feline').setup()
-vim.g['fern#renderer'] = 'nerdfont'
-vim.g['fern#default_hidden'] = '1'
-vim.g['fern#comparator'] = "lexical"
-require('gitsigns').setup()
 
 -- #############################
 -- # Trouble
 -- #############################
 local trouble=require("trouble")
-trouble.setup{}
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
-  {silent = true, noremap = true}
-)
-
+trouble.setup()
 
 -- #############################
 -- # Flutter
