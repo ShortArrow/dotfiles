211d76229e96f5b85da6876a464f06d82595ac37
shortarrow <bamboogeneral@shortarrow.jp>
2022-05-27 11:22:03 +0900

    fix_trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,31 @@ vim.g['fern#comparator'] = "lexical"
 require('gitsigns').setup()
 
 -- #############################
+-- # Trouble
+-- #############################
+local trouble=require("trouble")
+trouble.setup{}
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+
+
+-- #############################
 -- # Flutter
 -- #############################
 require("flutter-tools").setup{
@@ -46,79 +71,63 @@ require("flutter-tools").setup{
   }
 }
 local actions = require('telescope.actions')
-	local actions_set = require('telescope.actions.set')
-	local themes = require('telescope.themes')
+local actions_set = require('telescope.actions.set')
+local themes = require('telescope.themes')
 
-	local telescope = require('telescope')
+-- #############################
+-- # Telescope
+-- #############################
+local telescope = require('telescope')
 
-	telescope.setup{
-		defaults = {
-			layout = 'horizontal',
-			winblend = 20,
-			prompt_prefix = 'ﱢ ',
-			selection_caret = ' ',
-			entry_prefix = '  ',
-			initial_mode = 'insert',
-			border = false,
-			sorting_strategy = 'ascending',
-			path_display = { 'truncate' },
-			mappings = {
-				i = {
-					['<esc>'] = actions.close,
-					['<C-h>'] = 'which_key',
-					['<C-Down>'] = actions.cycle_history_next,
-					['<C-Up>'] = actions.cycle_history_prev,
-					['<C-j>'] = actions.cycle_history_next,
-					['<C-k>'] = actions.cycle_history_prev
-				}
-			},
-			layout_config = {
-				horizontal = {
-					height = 0.7,
-					preview_cutoff = 120,
-					preview_width = 50,
-					prompt_position = 'top',
-					width = 0.7,
-				},
-				cursor = {
-					height = 0.2,
-					preview_cutoff = 40,
-					width = 0.6
-				}
-			}
-		},
-		pickers = {
-			registers = {
-				theme = 'cursor'
-			},
-			lsp_code_actions = {
-				theme = 'cursor'
-			}
-		},
-		extensions = {}
-	}
-
-	telescope.load_extension "flutter"
--- Trouble
-require("trouble").setup {
-    vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
-    {silent = true, noremap = true}
-  )
+telescope.setup{
+  defaults = {
+    layout = 'horizontal',
+    winblend = 20,
+    prompt_prefix = 'ﱢ ',
+    selection_caret = ' ',
+    entry_prefix = '  ',
+    initial_mode = 'insert',
+    border = false,
+    sorting_strategy = 'ascending',
+    path_display = { 'truncate' },
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close,
+        ['<C-h>'] = 'which_key',
+        ['<C-Down>'] = actions.cycle_history_next,
+        ['<C-Up>'] = actions.cycle_history_prev,
+        ['<C-j>'] = actions.cycle_history_next,
+        ['<C-k>'] = actions.cycle_history_prev,
+	['<C-t>'] = trouble.open_with_trouble
+      },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+    layout_config = {
+      horizontal = {
+        height = 0.7,
+        preview_cutoff = 120,
+        preview_width = 50,
+        prompt_position = 'top',
+        width = 0.7,
+      },
+      cursor = {
+        height = 0.2,
+        preview_cutoff = 40,
+        width = 0.6
+      }
+    }
+  },
+  pickers = {
+    registers = {
+      theme = 'cursor'
+    },
+    lsp_code_actions = {
+      theme = 'cursor'
+    }
+  },
+  extensions = {}
 }
+telescope.load_extension "flutter"
+
 vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
 
