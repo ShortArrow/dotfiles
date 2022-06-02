ea1585d5c663ab7123779eba2aefb30d96f73c54
shortarrow <bamboogeneral@shortarrow.jp>
2022-06-02 12:18:12 +0900

    start_file_splitting

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-require'plugins'
+require('plugin-dependencies')
 
-vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
+vim.cmd[[autocmd BufWritePost plugin-dependencies.lua PackerCompile]]
 
--- nvim/lua/plugins-dependencies.lua
+++ nvim/lua/plugins-dependencies.lua
@@ -23,37 +23,9 @@ return require('packer').startup(function()
   -- ################################################
   use {
     'obaland/vfiler.vim',
+    requires = { 'obaland/vfiler-column-devicons', 'kyazdani42/nvim-web-devicons', 'ryanoasis/vim-devicons'},
     config = function()
-      require('vfiler/config').setup {
-        options = {
-          auto_cd = true,
-          auto_resize = true,
-          keep = true,
-          layout = 'left',
-          name = 'explorer',
-          width = 30,
-          columns = 'indent,icon,name',
-        },
-      }
-      -- Start by partially changing the configurations from the default.
-      local action = require'vfiler/action'
-      local configs = {
-        options = {
-          name = 'myfiler',
-          preview = {
-            layout = 'right',
-          },
-        },
-      
-        mappings = {
-          ['<C-l>'] = action.open_tree,
-          ['<C-h>'] = action.close_tree_or_cd,
-        },
-      }
-      
-      -- Start vfiler.vim
-      require'vfiler'.start(dirpath, configs)
-      -- require('vfiler').start()
+      require('vfiler').config()
     end,
   }
   use {
@@ -68,9 +40,7 @@ return require('packer').startup(function()
       'lambdalisue/fern.vim',
       'lambdalisue/nerdfont.vim'
     },
-    config = function()
-      vim.g['fern#renderer'] = 'nerdfont'
-    end,
+    config = require('fern-renderer-nerdfont').config(),
   }
   use {
     'lambdalisue/fern-git-status.vim',
@@ -83,9 +53,7 @@ return require('packer').startup(function()
     requires = {
       'lambdalisue/fern.vim',
     },
-    config = function()
-      vim.g['fern#comparator '] = 'lexical'
-    end,
+    config = require('fern-comparator-lexical').config(),
   }
   -- ################################################
   -- # Status line
@@ -95,7 +63,6 @@ return require('packer').startup(function()
     config = function()
       require('feline').setup()
     end,
-
   }
   use 'b0o/incline.nvim'
   -- ################################################
@@ -105,97 +72,20 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = {'nvim-lua/plenary.nvim', 'akinsho/flutter-tools.nvim'},
     config = function()
-      local telescope = require('telescope')
-      local actions = require('telescope.actions')
-      local actions_set = require('telescope.actions.set')
-      local themes = require('telescope.themes')
-      
-      telescope.setup{
-        defaults = {
-          layout = 'horizontal',
-          winblend = 20,
-          prompt_prefix = 'ﱢ ',
-          selection_caret = ' ',
-          entry_prefix = '  ',
-          initial_mode = 'insert',
-          border = false,
-          sorting_strategy = 'ascending',
-          path_display = { 'truncate' },
-          mappings = {
-            i = {
-              ['<esc>'] = actions.close,
-              ['<C-h>'] = 'which_key',
-              ['<C-Down>'] = actions.cycle_history_next,
-              ['<C-Up>'] = actions.cycle_history_prev,
-              ['<C-j>'] = actions.cycle_history_next,
-              ['<C-k>'] = actions.cycle_history_prev,
-            },
-            n = {  },
-          },
-          layout_config = {
-            horizontal = {
-              height = 0.7,
-              preview_cutoff = 120,
-              preview_width = 50,
-              prompt_position = 'top',
-              width = 0.7,
-            },
-            cursor = {
-              height = 0.2,
-              preview_cutoff = 40,
-              width = 0.6
-            }
-          }
-        },
-        pickers = {
-          registers = {
-            theme = 'cursor'
-          },
-          lsp_code_actions = {
-            theme = 'cursor'
-          }
-        },
-        extensions = {}
-      }
-      telescope.load_extension "flutter"
+      require('telescope').setup()
     end,
   }
   -- ################################################
   -- # Flutter
   -- ################################################
   use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
-  use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim',
+  use {
+    'akinsho/flutter-tools.nvim',
+    requires = {'nvim-lua/plenary.nvim'},
     config = function()
-      require("flutter-tools").setup{
-        lsp = {
-          color = {
-            enabled = true,
-            background = true, 
-            foreground = true,
-            virtual_text = true,
-            virtual_text_str = "■",
-          },
-          on_attach = function(client, bufnr)
-            vim.cmd [[hi FlutterWidgetGuides ctermfg=237 guifg=#33374c]]
-            vim.cmd [[hi ClosingTags ctermfg=244 guifg=#8389a3]]
-            on_attach(client, bufnr)
-          end,
-          capabilities = capabilities,
-          widget_guides = {
-            enabled = true,
-          },
-          debugger = {
-          enabled = true,
-            register_configurations = function(_)
-              require("dap").configurations.dart = {}
-              require("dap.ext.vscode").load_launchjs()
-            end,
-          },
-        }
-      }
+      require('flutter-tools').setup()
     end,
   }
-  -- with packer
   use 'mfussenegger/nvim-dap'
   -- ################################################
   -- # Git
