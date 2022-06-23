663a48d934b8b6c2bda07647db7562b9178e8104
shortarrow <bamboogeneral@shortarrow.jp>
2022-06-23 09:58:03 +0900

    fix_lsp

--- nvim/lua/config/_cmp.lua
+++ nvim/lua/config/_cmp.lua
@@ -81,6 +81,6 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+require('lspconfig')['dartls'].setup {
   capabilities = capabilities
 }
