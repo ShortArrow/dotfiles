b7b0f6d2ee44dc852dfb3d590d937efd2c94afe5
shortarrow <bamboogeneral@shortarrow.jp>
2022-06-29 14:54:29 +0900

    split_bash_config

---
+++ bash/bash_myplug.sh
@@ -0,0 +1,22 @@
+# mosh
+export LANG='en_US.UTF8'
+export LC_CTYPE='en_US.UTF8'
+
+# lua
+alias luamake=/home/verde/Documents/GitHub/lua-language-server/3rd/luamake/luamake
+
+# rust
+. "$HOME/.cargo/env"
+
+# vimmer terminal
+set -o vi
+export VISUAL=nvim
+export EDITOR=nvim
+unset LESSEDIT
+
+# starship
+eval "$(starship init bash)"
+
+# npm, node
+# export PATH=~/.npm-global/bin:$PATH
+
