8cb2b7f6e930a1b82153a15dfd5de4af4bae3112
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-02 15:24:37 +0900

    refactor_keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,18 +2,19 @@ local depends = require('depends')
 local debugger = require('debugger')
 
 local keymap = vim.api.nvim_set_keymap
+local default_conf = { noremap = true, silent = false }
 
 local M = {}
 
 local function _Fugitive()
-  keymap('n', '<Leader>ga',':Git add .<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>gb',':Git blame<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>gc',':Git commit -m ', { noremap = true, silent = false })
-  keymap('n', '<Leader>gd',':Gvdiffsplit<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>gll',':Gllog<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>glc',':Gclog<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>gp',':Git push<CR>', { noremap = true, silent = false })
-  keymap('n', '<Leader>gs',':Git status<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>ga',':Git add .<CR>', default_conf)
+  keymap('n', '<Leader>gb',':Git blame<CR>', default_conf)
+  keymap('n', '<Leader>gc',':Git commit -m ', default_conf)
+  keymap('n', '<Leader>gd',':Gvdiffsplit<CR>', default_conf)
+  keymap('n', '<Leader>gll',':Gllog<CR>', default_conf)
+  keymap('n', '<Leader>glc',':Gclog<CR>', default_conf)
+  keymap('n', '<Leader>gp',':Git push<CR>', default_conf)
+  keymap('n', '<Leader>gs',':Git status<CR>', default_conf)
   -- nnoremap <leader>ga :Git add %:p<CR><CR>
   -- nnoremap <leader>gc :Gcommit<CR><CR>
   -- nnoremap <leader>gs :Gstatus<CR>
@@ -26,30 +27,30 @@ end
 
 local function _Flutter()
   keymap('n', '<Leader>fr',':FlutterRun -d web-server<CR>',
-        { noremap = true, silent = false })
+        default_conf)
   keymap('n', '<Leader>fc',
         [[<Cmd>lua require('telescope').extensions.flutter.commands()<CR>]],
-        { noremap = true, silent = false })
+        default_conf)
   debugger.print("setuped flutter keymap")
 end
 
 local function _Trouble()
-  keymap('n', '<Leader>tt',':TroubleToggle<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>tt',':TroubleToggle<CR>', default_conf)
   debugger.print("setuped trouble keymap")
 end
 
 local function _FzfLua()
-  keymap('n', '<Leader>zf',':FzfLua files<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>zf',':FzfLua files<CR>', default_conf)
   debugger.print("setuped fzflua keymap")
 end
 
 local function _Packer()
-  keymap('n', '<Leader>ps',':PackerSync<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>ps',':PackerSync<CR>', default_conf)
   debugger.print("setuped packer keymap")
 end
 
 local function _VFiler()
-  keymap('n', '<Leader>vf',':VFiler<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>vf',':VFiler<CR>', default_conf)
   debugger.print("setuped vfiler keymap")
 end
 
