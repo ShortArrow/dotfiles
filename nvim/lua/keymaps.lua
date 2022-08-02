5077eff613456345c248586677d93f8387385177
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-02 15:04:54 +0900

    refactor_keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,17 +1,19 @@
 local depends = require('depends')
 local debugger = require('debugger')
 
+local keymap = vim.api.nvim_set_keymap
+
 local M = {}
 
 local function _Fugitive()
-  vim.api.nvim_set_keymap('n', '<Leader>ga',':Git add .<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gb',':Git blame<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gc',':Git commit -m ', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gd',':Gvdiffsplit<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gll',':Gllog<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>glc',':Gclog<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gp',':Git push<CR>', { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>gs',':Git status<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>ga',':Git add .<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>gb',':Git blame<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>gc',':Git commit -m ', { noremap = true, silent = false })
+  keymap('n', '<Leader>gd',':Gvdiffsplit<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>gll',':Gllog<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>glc',':Gclog<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>gp',':Git push<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>gs',':Git status<CR>', { noremap = true, silent = false })
   -- nnoremap <leader>ga :Git add %:p<CR><CR>
   -- nnoremap <leader>gc :Gcommit<CR><CR>
   -- nnoremap <leader>gs :Gstatus<CR>
@@ -23,31 +25,31 @@ local function _Fugitive()
 end
 
 local function _Flutter()
-  vim.api.nvim_set_keymap('n', '<Leader>fr',':FlutterRun -d web-server<CR>',
+  keymap('n', '<Leader>fr',':FlutterRun -d web-server<CR>',
         { noremap = true, silent = false })
-  vim.api.nvim_set_keymap('n', '<Leader>fc',
+  keymap('n', '<Leader>fc',
         [[<Cmd>lua require('telescope').extensions.flutter.commands()<CR>]],
         { noremap = true, silent = false })
   debugger.print("setuped flutter keymap")
 end
 
 local function _Trouble()
-  vim.api.nvim_set_keymap('n', '<Leader>tt',':TroubleToggle<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>tt',':TroubleToggle<CR>', { noremap = true, silent = false })
   debugger.print("setuped trouble keymap")
 end
 
 local function _FzfLua()
-  vim.api.nvim_set_keymap('n', '<Leader>zf',':FzfLua files<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>zf',':FzfLua files<CR>', { noremap = true, silent = false })
   debugger.print("setuped fzflua keymap")
 end
 
 local function _Packer()
-  vim.api.nvim_set_keymap('n', '<Leader>ps',':PackerSync<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>ps',':PackerSync<CR>', { noremap = true, silent = false })
   debugger.print("setuped packer keymap")
 end
 
 local function _VFiler()
-  vim.api.nvim_set_keymap('n', '<Leader>vf',':VFiler<CR>', { noremap = true, silent = false })
+  keymap('n', '<Leader>vf',':VFiler<CR>', { noremap = true, silent = false })
   debugger.print("setuped vfiler keymap")
 end
 
