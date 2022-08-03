d84126e6f7a27c5087f6b60e4b2b618d2e9f9345
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-03 10:24:43 +0900

    add_tmux_setupsh

---
+++ tmux/setup.sh
@@ -0,0 +1,15 @@
+# make directory of tmux configrations
+mkdir -p ~/.tmux
+
+# download color theme icebarg theme fork
+wget -O $HOME/.tmux/iceberg_minimal_with_win_index.tmux.conf \
+https://raw.githubusercontent.com/ShortArrow/iceberg-dark/master/.tmux/iceberg_minimal_with_win_index.tmux.conf
+
+# make symbolic link
+rm -rf ~/.tmux_myplug
+ln -s $HOME/Documents/GitHub/dotfiles/tmux/tmux_myplug.sh ~/.tmux_myplug # caution! Don't needs slash at last.
+file ~/.tmux_myplug # check link
+
+# display guides after run this script
+echo "Write this at the end of ~/.tmux.conf"
+echo "source ~/.tmux_myplug"
