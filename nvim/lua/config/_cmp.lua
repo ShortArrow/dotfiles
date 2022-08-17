c256ff8df1d568abe4e0a26250748f9f789d4e88
ShortArrow <bamboogeneral@shortarrow.jp>
2022-08-18 07:44:21 +0900

    format

--- nvim/lua/config/_cmp.lua
+++ nvim/lua/config/_cmp.lua
@@ -16,8 +16,8 @@ M.setup = function()
       end,
     },
     -- window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
     -- },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -26,7 +26,7 @@ M.setup = function()
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Replace,
-        select = true  -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        select = true -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       ['<Tab>'] = function(fallback)
         if cmp.visible() then
@@ -58,7 +58,7 @@ M.setup = function()
         maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
         -- The function below will be called before any actual modifications from lspkind
         -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function (entry, vim_item)
+        before = function(entry, vim_item)
           return vim_item
         end
       })
