22460efea07e31bd9e9883e07b8c06e4bc3d5028
ShortArrow <bamboogeneral@shortarrow.jp>
2022-08-17 22:37:17 +0900

    fix

--- nvim/lua/packer-depends.lua
+++ nvim/lua/packer-depends.lua
@@ -130,7 +130,6 @@ return require('packer').startup(function()
   use {
     "hrsh7th/nvim-cmp",
     requires = {
-      'neovim/nvim-lspconfig',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
