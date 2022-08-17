f1aa9df93ceaf007a9268d7a0c4688702ee0d6e2
ShortArrow <bamboogeneral@shortarrow.jp>
2022-08-18 04:46:48 +0900

    change_indent

--- nvim/lua/config/_indent.lua
+++ nvim/lua/config/_indent.lua
@@ -4,12 +4,12 @@ M.setup = function()
   local _indent_blankline = require("indent_blankline")
 
   vim.opt.list = true
-  vim.opt.listchars:append "space:⋅"
   vim.opt.listchars:append "eol:↴"
+  -- vim.opt.listchars:append "space:⋅"
 
   _indent_blankline.setup {
     show_end_of_line = true,
-    space_char_blankline = " ",
+    -- space_char_blankline = " ",
   }
 end
 
