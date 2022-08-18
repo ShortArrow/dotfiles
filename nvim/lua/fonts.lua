458135975de7d096e285d7db43d34d6ea2a45a13
ShortArrow <bamboogeneral@shortarrow.jp>
2022-08-18 09:11:07 +0900

    fix

--- nvim/lua/fonts.lua
+++ nvim/lua/fonts.lua
@@ -2,29 +2,33 @@ local debugger = require('debugger')
 
 local M = {}
 
-M.list = {
-  'PlemolJPConsoleNF',
-  'BlexMono Nerd Font',
-  'RbotoJ',
-  'cascadia code',
-  'Fira Code',
-  'Source Code Pro',
-  'Consolas',
-  'Courier New',
-  'monospace',
-}
 
 M.get_fonts = function()
+  local list = {
+    'PlemolJPConsoleNF',
+    'BlexMono Nerd Font',
+    'RbotoJ',
+    'cascadia code',
+    'Fira Code',
+    'Source Code Pro',
+    'Consolas',
+    'Courier New',
+    'monospace',
+  }
   local line
-  for key, value in pairs(M.list) do
+  for key, value in pairs(list) do
+    if string.find(value, ' ') then
+      value = '\'' .. value .. '\''
+    end
     if key == 1 then
       line = value
     else
-      line = line .. value
+      line = line .. ',' .. value
     end
   end
-  debugger.print('fonts: ' .. line)
   return line
 end
+
+debugger.print('fonts: "' .. M.get_fonts() .. '"')
 
 return M
