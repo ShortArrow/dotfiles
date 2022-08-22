2b46e44b4180cd1e953081d1b032444e1ad6347b
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-22 09:37:13 +0900

    add_bashlsp

--- nvim/lua/config/_mason.lua
+++ nvim/lua/config/_mason.lua
@@ -47,6 +47,11 @@ M.setup = function()
             },
           }
         }
+      elseif server_name == "bashls" then
+        _opts.capabilities = capabilities
+        _opts.on_attach = function(signature_setup, bufnr)
+          _lsp_sig.on_attach(signature_setup, bufnr)
+        end
       end
       _nvim_lsp[server_name].setup(_opts)
     end
