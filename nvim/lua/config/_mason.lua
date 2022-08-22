81a97de572723541818d227b26f3bf930f6749c0
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-22 16:22:07 +0900

    add_jsonls

--- nvim/lua/config/_mason.lua
+++ nvim/lua/config/_mason.lua
@@ -77,6 +77,11 @@ M.setup = function()
         _opts.on_attach = function(signature_setup, bufnr)
           _lsp_sig.on_attach(signature_setup, bufnr)
         end
+      elseif server_name == "jsonls" then
+        _opts.capabilities = capabilities
+        _opts.on_attach = function(signature_setup, bufnr)
+          _lsp_sig.on_attach(signature_setup, bufnr)
+        end
       end
       _nvim_lsp[server_name].setup(_opts)
     end
