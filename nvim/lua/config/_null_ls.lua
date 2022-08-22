1e34d0aa0dde3a95c6c986a80bbd9e54b0eda317
shortarrow <bamboogeneral@shortarrow.jp>
2022-08-22 17:01:09 +0900

    fix_nullls

--- nvim/lua/config/_null_ls.lua
+++ nvim/lua/config/_null_ls.lua
@@ -1,12 +1,35 @@
-local M ={}
+local ok, null_ls = pcall(require, "null-ls")
+local debugger = require('debugger')
+if not ok then
+  debugger.print('null-ls is null')
+  return
+end
 
-M.setup = function ()
-  require("null-ls").setup{
+local M = {}
+
+M.setup = function()
+  local editorconfig_checker = null_ls.builtins.diagnostics.editorconfig_checker
+  editorconfig_checker._opts.command = "editorconfig-checker"
+  null_ls.setup {
     sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
+      editorconfig_checker,
+      null_ls.builtins.diagnostics.actionlint,
+      null_ls.builtins.diagnostics.codespell,
+      null_ls.builtins.diagnostics.gitlint,
+      null_ls.builtins.diagnostics.misspell,
+      null_ls.builtins.diagnostics.selene,
+      null_ls.builtins.diagnostics.shellcheck,
+      null_ls.builtins.diagnostics.yamllint,
+      null_ls.builtins.formatting.jq,
+      null_ls.builtins.formatting.ktlint,
+      null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.shellharden,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.completion.spell,
     },
+    -- on_attach = require "wb.lsp.on-attach",
   }
 end
 
