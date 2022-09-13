04842c193f5089e991b050558370ca54c69c34e7
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-13 15:23:06 +0900

    change_to_api_biggining

--- nvim/lua/depends.lua
+++ nvim/lua/depends.lua
@@ -1,15 +1,12 @@
 local keymaps = require('keymaps')
-
-local is_win_os = function()
-  return "Windows_NT" ~= vim.loop.os_uname().sysname
-end
+local api = require('myapi')
 
 local M = {
   common   = { enable = true, name = 'common', keymaps = keymaps.Common },
   fugitive = { enable = true, name = 'fugitive', keymaps = keymaps.Fugitive },
   flutter  = { enable = true, name = 'flutter', keymaps = keymaps.Flutter },
   trouble  = { enable = true, name = 'trouble', keymaps = keymaps.Trouble },
-  fzflua   = { enable = is_win_os(),
+  fzflua   = { enable = api.env.is_win_os(),
     name = 'fzflua', keymaps = keymaps.FzfLua },
   packer   = { enable = true, name = 'packer', keymaps = keymaps.Packer },
   vfiler   = { enable = true, name = 'vfiler', keymaps = keymaps.VFiler },
