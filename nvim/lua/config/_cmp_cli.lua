67501367be4463f3f3ad47653ee33ca5f31ed0b8
ShortArrow <bamboogeneral@live.jp>
2022-09-13 12:30:18 +0900

    fix_feline

---
+++ nvim/lua/config/_cmp_cli.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.setup = function()
+  -- require('cmp-cmdline').setup.cmdline(':', {
+  --   sources = cmp.config.sources({
+  --     { name = 'path' }
+  --   }, {
+  --     { name = 'cmdline', keyword_pattern=[=[[^[:blank:]\!]*]=]}
+  --   })
+  -- })
+end
+
+return M
