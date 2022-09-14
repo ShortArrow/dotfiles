9f78c41db2d50e20877fd5cc73d803faad6f2ab7
ShortArrow <bamboogeneral@shortarrow.jp>
2022-09-15 04:41:44 +0900

    linenum_rel

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ local ignition = require('my/ignition')
 local fonts = require('my/fonts')
 
 vim.o.number = true
+vim.o.relativenumber = true
 vim.o.termguicolors = true
 vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
