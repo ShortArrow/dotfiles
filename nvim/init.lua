ad63b7dc80e41e6f30a53553564008d99d86d552
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-19 08:32:58 +0900

    change fold icon

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,7 +19,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- packer from here
 -- https://github.com/wbthomason/dotfiles/tree/linux/neovim/.config/nvim
 function P(...)
