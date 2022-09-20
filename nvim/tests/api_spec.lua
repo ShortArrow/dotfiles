23c4f6e460d741a10a5e90cce3ee2afc361be872
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-20 10:34:30 +0900

    refactor_keymaps

---
+++ nvim/tests/api_spec.lua
@@ -0,0 +1,34 @@
+describe('api test', function()
+
+  local utils = require('my/api').utils
+
+  before_each(function()
+  end)
+
+  it('function is true', function()
+    local sample = function()
+    end
+    assert.equals(true, utils.is_function(sample))
+  end)
+
+  it('not function is false', function()
+    local sample = nil
+    assert.equals(false, utils.is_function(sample))
+  end)
+
+  it('boolean is true', function()
+    local sample = true
+    assert.equals(true, utils.is_boolean(sample))
+  end)
+
+  it('returned boolean is true', function()
+    local sample = function() return false end
+    assert.equals(true, utils.is_boolean(sample()))
+  end)
+
+  it('not boolean is false', function()
+    local sample = nil
+    assert.equals(false, utils.is_boolean(sample))
+  end)
+
+end)
