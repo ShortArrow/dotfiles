5bad85115234b2fd464de64b5c0f21ce44b35a96
ShortArrow <bamboogeneral@live.jp>
2022-09-20 16:16:50 +0900

    fix_pyright

--- nvim/lua/lang/python.lua
+++ nvim/lua/lang/python.lua
@@ -24,10 +24,17 @@ M.pyls = {
 
 M.env = function()
   -- python path https://imokuri123.com/blog/2017/07/neovim-python-virtualenv/
+  vim.g.loaded_python_provider = 0
   if api.env.is_win_os() then
-    vim.g.python3_host_prog = './.venv/Script/python'
+    vim.g.python_dir = ".\\.venv\\Scripts\\"
+    vim.g.python3_dir = ".\\.venv\\Scripts\\"
+    vim.g.python_host_prog = vim.g.python_dir .. 'python.exe'
+    vim.g.python3_host_prog = vim.g.python3_dir .. 'python.exe'
   else
-    vim.g.python3_host_prog = './.venv/bin/python'
+    vim.g.python_dir = './.venv/bin/'
+    vim.g.python3_dir = './.venv/bin/'
+    vim.g.python_host_prog = vim.g.python_dir .. 'python'
+    vim.g.python3_host_prog = vim.g.python3_dir .. 'python'
   end
 end
 
