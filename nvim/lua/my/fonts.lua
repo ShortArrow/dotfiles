a93057e68e2b849b0048c480d36b9647a88d15e5
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-20 10:37:56 +0900

    cleanup_fonts

--- nvim/lua/my/fonts.lua
+++ nvim/lua/my/fonts.lua
@@ -1,8 +1,6 @@
-local debugger = require('my/debugger')
-
 local M = {}
 
-local concatinate_fontnames = function (list)
+M.concatinate_fontnames = function (list)
   local line
   for key, value in pairs(list) do
     if string.find(value, ' ') then
@@ -29,9 +27,7 @@ M.get_fonts = function()
     'Courier New',
     'monospace',
   }
-  return concatinate_fontnames(list)
+  return M.concatinate_fontnames(list)
 end
-
-debugger.print('fonts: "' .. M.get_fonts() .. '"')
 
 return M
