60e63b127752dda3f34baa248eeec9b630995d07
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-21 09:18:41 +0900

    tmux_color

--- bash/bash_myplug.sh
+++ bash/bash_myplug.sh
@@ -66,6 +66,9 @@ checkcolor(){
   curl -s 'https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh' | bash
 }
 
+# tmux color
+alias tmux="tmux -2"
+
 # sshrc
 # https://github.com/rcmdnk/sshrc
 # alias ssh="sshrc"
