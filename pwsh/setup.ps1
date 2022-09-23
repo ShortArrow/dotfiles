73ee5208261b67cddb0fdd7983188c6ff674d8bd
shortarrow <bamboogeneral@shortarrow.jp>
2022-09-23 15:30:42 +0900

    fix_pwsh_prof

--- pwsh/setup.ps1
+++ pwsh/setup.ps1
@@ -1,5 +1,8 @@
 $ProfileDir=(Split-Path $PROFILE -Parent)
-Remove-Item (Resolve-Path $ProfileDir/pwsh_myplug.ps1) -Force
-New-Item -Type SymbolicLink -Path "$ProfileDir" -Name "pwsh_myplug.ps1" -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/pwsh_myplug.ps1"
+if (Test-Path "$ProfileDir/pwsh_myplug.ps1"){
+  Remove-Item "$ProfileDir/pwsh_myplug.ps1" -Force
+}
+New-Item -Type SymbolicLink -Path "$ProfileDir" -Name "pwsh_myplug.ps1" -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/pwsh/pwsh_myplug.ps1"
 Get-ChildItem -Path $env:LOCALAPPDATA -Force -ErrorAction 'silentlycontinue' |
-Where-Object { $_.Attributes -match "ReparsePoint" -and $_.Name -match "nvim"}
+Where-Object { $_.Attributes -match "ReparsePoint" -and $_.Name -match "pwsh_myplug"}
+echo "Write '. `"`$(Split-Path -Parent `$PROFILE)/pwsh_myplug.ps1`"' in `$PROFILE"
