e71d42d175fa509742536e85c0509177bf2d11af
ShortArrow <bamboogeneral@shortarrow.jp>
2022-10-10 07:14:52 +0900

    fix_tmux_shortcut

--- tmux/tmux_myplug.sh
+++ tmux/tmux_myplug.sh
@@ -14,7 +14,7 @@ set-option -sg escape-time 10
 set-option -g focus-events on
 
 # key bind of window switching
-bind h select-pane -L
-bind j select-pane -D
-bind k select-pane -U
-bind l select-pane -R
+bind -n M-h select-pane -L
+bind -n M-j select-pane -D
+bind -n M-k select-pane -U
+bind -n M-l select-pane -R
