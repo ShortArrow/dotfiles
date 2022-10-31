e5992498410586fb3663e5aac99375d0f0c3d29c
shortarrow <bamboogeneral@shortarrow.jp>
2022-10-31 17:13:52 +0900

    cursorline

---
+++ nvim/lua/config/_cursorline.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.setup = function()
+  require('nvim-cursorline').setup {
+    cursorline = {
+      enable = true,
+      timeout = 1000,
+      number = false,
+    },
+    cursorword = {
+      enable = true,
+      min_length = 3,
+      hl = { underline = true },
+    }
+  }
+end
+
+return M
