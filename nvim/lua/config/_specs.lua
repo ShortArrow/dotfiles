0bcf96c4f97ac6bc61ded45493a221b68e50967f
shortarrow <bamboogeneral@shortarrow.jp>
2022-10-31 17:52:39 +0900

    add_specs

---
+++ nvim/lua/config/_specs.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.setup = function()
+  require('specs').setup {
+    show_jumps       = true,
+    min_jump         = 10,
+    popup            = {
+      delay_ms = 0, -- delay before popup displays
+      inc_ms = 1, -- time increments used for fade/resize effects
+      blend = 1, -- starting blend, between 0-100 (fully transparent), see :h winblend
+      width = 100,
+      winhl = "PMenu",
+      fader = require('specs').pulse_fader,
+      resizer = require('specs').shrink_resizer
+    },
+    ignore_filetypes = {},
+    ignore_buftypes  = {
+      nofile = true,
+    },
+  }
+end
+
+return M
