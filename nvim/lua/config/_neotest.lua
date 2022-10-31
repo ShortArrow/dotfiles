b1aaf7883417c0dde08e205a8c0a5b20cc93894b
shortarrow <bamboogeneral@shortarrow.jp>
2022-10-31 18:36:36 +0900

    add_neotest

---
+++ nvim/lua/config/_neotest.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+M.setup = function()
+  require("neotest").setup({
+    adapters = {
+      require("neotest-python")({
+        dap = { justMyCode = false },
+      }),
+      require("neotest-plenary"),
+      require("neotest-vim-test")({
+        ignore_file_types = { "python", "vim", "lua" },
+      }),
+    },
+  })
+end
+
+return M
+-- | pytest          |   [neotest-python](https://github.com/nvim-neotest/neotest-python)   |
+-- | python-unittest |   [neotest-python](https://github.com/nvim-neotest/neotest-python)   |
+-- | plenary         |  [neotest-plenary](https://github.com/nvim-neotest/neotest-plenary)  |
+-- | go              |         [neotest-go](https://github.com/akinsho/neotest-go)          |
+-- | jest            |     [neotest-jest](https://github.com/haydenmeade/neotest-jest)      |
+-- | vitest          |     [neotest-vitest](https://github.com/marilari88/neotest-vitest)   |
+-- | rspec           |     [neotest-rspec](https://github.com/olimorris/neotest-rspec)      |
+-- | dart, flutter   |       [neotest-dart](https://github.com/sidlatau/neotest-dart)       |
+-- | testthat        | [neotest-testthat](https://github.com/shunsambongi/neotest-testthat) |
+-- | phpunit         |   [neotest-phpunit](https://github.com/olimorris/neotest-phpunit)    |
+-- | rust            |        [neotest-rust](https://github.com/rouge8/neotest-rust)        |
+-- | elixir          |    [neotest-elixir](https://github.com/jfpedroza/neotest-elixir)     |
+-- | dotnet          |    [neotest-dotnet](https://github.com/Issafalcon/neotest-dotnet)    |
+-- | scala           |    [neotest-scala](https://github.com/stevanmilic/neotest-scala)     |
+-- | haskell         |    [neotest-haskell](https://github.com/mrcjkb/neotest-haskell)      |
