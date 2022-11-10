81b47eb3e336468f0a5a63dbccfd5d3597be5537
shortarrow <bamboogeneral@shortarrow.jp>
2022-11-11 01:05:11 +0900

    mouse_right_click

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,6 +20,7 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.mousemodel = 'extend'
 -- packer from here
 -- https://github.com/wbthomason/dotfiles/tree/linux/neovim/.config/nvim
 function P(...)
@@ -45,4 +46,3 @@ ignition.start()
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
