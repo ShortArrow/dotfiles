2b7be00c8ad79b93111a9711c7d757e52298cca8
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-09 12:30:13 +0900

    fix

--- nvim/lua/config/_fern.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.setup = function()
-  vim.g['fern#default_hidden'] = '1'
-end
-
-return M
--- nvim/lua/config/_mason.lua
+++ nvim/lua/config/_mason.lua
@@ -30,8 +30,6 @@ M.setup = function()
   }
   _mason_lspconfig.setup_handlers({
     function(server_name)
-      local node_root_dir = _nvim_lsp.util.root_pattern("package.json")
-      local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
       local _opts = {}
       _opts.capabilities = capabilities
       _opts.on_attach = function(_, bufnr)
@@ -39,22 +37,12 @@ M.setup = function()
       end
       if server_name == "sumneko_lua" then
         _opts.settings = _api.lang.lua.sumneko_lua
-      elseif server_name == "tsserver" then
-        if not is_node_repo then
-          return
-        end
-        _opts.root_dir = node_root_dir
+      elseif server_name == "typescript-language-server" then
+        _opts.root_dir = _nvim_lsp.util.root_pattern("package.json")
       elseif server_name == "eslint" then
-        if not is_node_repo then
-          return
-        end
-        _opts.root_dir = node_root_dir
+        _opts.root_dir = _nvim_lsp.util.root_pattern("package.json")
       elseif server_name == "denols" then
-        if is_node_repo then
-          return
-        end
-
-        _opts.root_dir = _nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
+        _opts.root_dir = _nvim_lsp.util.root_pattern("deno.json")
         _opts.settings = _api.lang.deno.denols
         -- _opts.init_options = {
         --   lint = true,
