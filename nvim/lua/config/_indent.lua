4571799af5b3b28407d68bd6aa69f29156da874e
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-15 19:47:05 +0900

    indent_color

--- nvim/lua/config/_indent.lua
+++ nvim/lua/config/_indent.lua
@@ -3,13 +3,28 @@ local M = {}
 M.setup = function()
   local _indent_blankline = require("indent_blankline")
 
+  -- vim.opt.termguicolors = true
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent1', { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent2', { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent3', { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent4', { fg = "#56B6C2" })
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = "#C678DD" })
   vim.opt.list = true
   vim.opt.listchars:append "eol:↴"
-  -- vim.opt.listchars:append "space:⋅"
+  vim.opt.listchars:append "space:⋅"
 
   _indent_blankline.setup {
     show_end_of_line = true,
-    -- space_char_blankline = " ",
+    space_char_blankline = " ",
+    char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+    },
   }
 end
 
