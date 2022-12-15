250a4b1fd5cee953b98538d63de395519cfd04e3
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-15 22:01:58 +0900

    fix_indent_color

--- nvim/lua/config/_indent.lua
+++ nvim/lua/config/_indent.lua
@@ -4,26 +4,28 @@ M.setup = function()
   local _indent_blankline = require("indent_blankline")
 
   -- vim.opt.termguicolors = true
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent1', { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent2', { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent3', { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent4', { fg = "#56B6C2" })
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = "#C678DD" })
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent1', { fg = "#E06C75" }) -- rainbow
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent2', { fg = "#E5C07B" }) -- rainbow
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent3', { fg = "#98C379" }) -- rainbow
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent4', { fg = "#56B6C2" }) -- rainbow
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = "#61AFEF" }) -- rainbow
+  -- vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = "#C678DD" }) -- rainbow
   vim.opt.list = true
   -- vim.opt.listchars:append "eol:↴"
   -- vim.opt.listchars:append "space:"
   _indent_blankline.setup {
     show_end_of_line = true,
     space_char_blankline = " ",
-    char_highlight_list = {
-      "IndentBlanklineIndent1",
-      "IndentBlanklineIndent2",
-      "IndentBlanklineIndent3",
-      "IndentBlanklineIndent4",
-      "IndentBlanklineIndent5",
-      "IndentBlanklineIndent6",
-    },
+    show_current_context = true,
+    show_current_context_start = true,
+    -- char_highlight_list = { -- rainbow
+    --   "IndentBlanklineIndent1", -- rainbow
+    --   "IndentBlanklineIndent2", -- rainbow
+    --   "IndentBlanklineIndent3", -- rainbow
+    --   "IndentBlanklineIndent4", -- rainbow
+    --   "IndentBlanklineIndent5", -- rainbow
+    --   "IndentBlanklineIndent6", -- rainbow
+    -- },
   }
 end
 
