00c03a0aca81025de8bcedfe9b882811c525ce51
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-15 21:24:18 +0900

    fillchars

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,44 @@
 local ignition = require('my.ignition')
 local fonts = require('my.fonts')
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
-vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
-vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
-vim.o.guifont = fonts.get_fonts()
-vim.o.encoding = 'UTF-8'
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.clipboard:append({ unnamedeplus = true })
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+vim.opt.guifont = fonts.get_fonts()
+vim.opt.encoding = 'UTF-8'
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.g.mapleader = " "
-vim.o.pumblend = 5
-vim.o.foldmethod = 'indent'
-vim.o.foldcolumn = '1'
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.mousemodel = 'extend'
+vim.opt.pumblend = 5
+vim.opt.foldmethod = 'indent'
+vim.opt.foldcolumn = '1'
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+vim.opt.mousemodel = 'extend'
+vim.opt.visualbell = true
+vim.opt.helplang = 'ja' --, 'en'
+vim.opt.fillchars = {
+  vert = "│",
+  fold = "f",
+  eob = "e", -- suppress ~ at EndOfBuffer
+  diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+  msgsep = "‾",
+  foldopen = "",
+  foldsep = "│",
+  foldclose = "",
+}
+vim.opt.listchars = {
+  eol = '⤶',
+  space = '·',
+  trail = '-',
+  extends = '◀',
+  precedes = '▶',
+}
 
 if not pcall(require, "impatient") then
   print "Failed to load impatient."
--- nvim/lua/config/_indent.lua
+++ nvim/lua/config/_indent.lua
@@ -11,9 +11,8 @@ M.setup = function()
   vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = "#61AFEF" })
   vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = "#C678DD" })
   vim.opt.list = true
-  vim.opt.listchars:append "eol:↴"
-  vim.opt.listchars:append "space:⋅"
-
+  -- vim.opt.listchars:append "eol:↴"
+  -- vim.opt.listchars:append "space:"
   _indent_blankline.setup {
     show_end_of_line = true,
     space_char_blankline = " ",
