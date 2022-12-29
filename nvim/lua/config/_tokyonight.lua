bb166f66e3a4f0f343dae89ce0fcece12055b78e
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-30 02:57:16 +0900

    tokyonight

--- nvim/lua/config/_tokyonight.lua
+++ nvim/lua/config/_tokyonight.lua
@@ -2,13 +2,32 @@ local M = {}
 
 function M.setup()
   local tn = require('tokyonight')
-  tn.setup()
-  vim.g.tokyonight_style = "night"
-  vim.g.tokyonight_italic_functions = true
-  vim.g.tokyonight_dark_sidebar = true
-  vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-  vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-  vim.cmd [[colorscheme tokyonight]]
+  tn.setup {
+    style = "night",
+    light_style = "day", -- The theme is used when the background is set to light
+    transparent = false, -- Enable this to disable setting the background color
+    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+    styles = {
+      -- Style to be applied to different syntax groups
+      -- Value is any valid attr-list value for `:help nvim_set_hl`
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+      -- Background styles. Can be "dark", "transparent" or "normal"
+      sidebars = "dark", -- style for sidebars, see below
+      floats = "dark", -- style for floating windows
+    },
+    sidebars = { "qf", "vista_kind", "terminal", "packer" }, -- Set a darker background on sidebar-like windows.
+    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+    dim_inactive = false, -- dims inactive windows
+    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    on_colors = function(colors)
+      colors.hint = colors.orange
+      colors.error = "#ff0000"
+    end
+  }
 end
 
 return M
