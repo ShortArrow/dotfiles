9794853a95239b97fa0f165e590425fe03ee9767
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-30 13:57:05 +0900

    conflict

--- nvim/lua/my/options.lua
+++ nvim/lua/my/options.lua
@@ -10,7 +10,6 @@ M.activate = function()
   opt.termguicolors  = true
   opt.ignorecase     = true -- (default : off)
   opt.smartcase      = true -- (default : off)
-  opt.smartcase      = true -- (default : off)
   opt.clipboard      = "unnamedplus"
   opt.guicursor      = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
   opt.guifont        = fonts.get_fonts()
