0b65a4985206e95b0a9dbe0bcb412ef649f585bb
ShortArrow <bamboogeneral@shortarrow.jp>
2022-12-30 14:49:19 +0900

    fix string

--- bash/bash_myplug.sh
+++ bash/bash_myplug.sh
@@ -1,32 +1,32 @@
 # ls
-alias l.='ls -d .* --color=tty'
-alias ll='ls -l --color=tty'
-alias ll.='ls -al --color=tty'
-alias ls='ls --color=tty'
-alias ls.='ls -a --color=tty'
+alias l.="ls -d .* --color=tty"
+alias ll="ls -l --color=tty"
+alias ll.="ls -al --color=tty"
+alias ls="ls --color=tty"
+alias ls.="ls -a --color=tty"
 
 # sudo refresher
-alias sudo='sudo -v; sudo'
+alias sudo="sudo -v; sudo"
 
 # ignore case on completion
-bind 'set completion-ignore-case on'
+bind "set completion-ignore-case on"
 
 # mosh
-export LANG='en_US.UTF8'
-export LC_CTYPE='en_US.UTF8'
+export LANG="en_US.UTF8"
+export LC_CTYPE="en_US.UTF8"
 
 # lua
-alias luamake=$HOME/Documents/GitHub/lua-language-server/3rd/luamake/luamake
-export PATH=$PATH:$HOME/Documents/GitHub/lua-language-server/bin
+alias luamake="$HOME/Documents/GitHub/lua-language-server/3rd/luamake/luamake"
+export PATH="$PATH:$HOME/Documents/GitHub/lua-language-server/bin"
 
 # rust
 . "$HOME/.cargo/env"
-export PATH=$PATH:$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rust-analyzer
+export PATH="$PATH:$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rust-analyzer"
 
 # vimmer terminal
 set -o vi
-export VISUAL=vi
-export EDITOR=vi
+export VISUAL=vim
+export EDITOR=vim
 unset LESSEDIT
 
 # starship
@@ -43,21 +43,21 @@ export PATH="$PATH:$HOME/Documents/GitHub/flutter/bin"
 export CHROME_EXECUTABLE="/usr/bin/chromium"
 
 # crontab
-alias crontab='crontab -i'
+alias crontab="crontab -i"
 
 # git-foresta
-alias foresta='git-foresta --all | less -RSX'
+alias foresta="git-foresta --all | less -RSX"
 
 # git-graph
-alias gg='git-graph --color always | less -RSX'
+alias gg="git-graph --color always | less -RSX"
 
 # lazydocker
-alias lzd='$HOME/.local/bin/lazydocker'
-alias lazydocker='$HOME/.local/bin/lazydocker'
+alias lzd="$HOME/.local/bin/lazydocker"
+alias lazydocker="$HOME/.local/bin/lazydocker"
 
 # lazygit
-alias lg='$HOME/Documents/GitHub/lazygit/main'
-alias lazygit='$HOME/Documents/GitHub/lazygit/main'
+alias lg="$HOME/Documents/GitHub/lazygit/main"
+alias lazygit="$HOME/Documents/GitHub/lazygit/main"
 
 # japanese
 export GTK_IM_MODULE=ibus
@@ -66,7 +66,7 @@ export MODIFIERS=@im=ibus
 
 # color check
 checkcolor(){
-  curl -s 'https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh' | bash
+  curl -s "https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh" | bash
 }
 
 # tmux color
@@ -77,10 +77,10 @@ alias tmux="tmux -2"
 # alias ssh="sshrc"
 
 # arduino-cli
-alias acli='$HOME/bin/arduino-cli'
+alias acli="$HOME/bin/arduino-cli"
 
 # fnm
-export PATH=/home/who/.fnm:$PATH
+export PATH="$HOME/.fnm:$PATH"
 eval "`fnm env`"
 
 # dvm
@@ -91,4 +91,4 @@ export DVM_DIR="/home/who/.dvm"
 export PATH="$DVM_DIR/bin:$PATH"
 
 # lunarvim
-export lvim="$HOME/.local/bin/lvim"
+alias lvim="$HOME/.local/bin/lvim"
