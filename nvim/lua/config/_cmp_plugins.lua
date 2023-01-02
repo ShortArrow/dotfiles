00da58cc2d5ec37e5d64e89e0c73a887fad91ed0
ShortArrow <bamboogeneral@shortarrow.jp>
2023-01-02 21:10:55 +0900

    fix cmp plugings

--- nvim/lua/config/_cmp_plugins.lua
+++ nvim/lua/config/_cmp_plugins.lua
@@ -1,9 +1,15 @@
 local M = {}
 
+local test = require('cmp-plugins')
 M.setup = function()
   require("cmp-plugins").setup({
-    files = { ".*\\.lua" } -- default
+    -- files = { ".*\\.lua" } -- default
     -- files = { "plugins.lua", "some_path/plugins/" } -- Recommended: use static filenames or partial paths
+    files = {
+      vim.fn.resolve(vim.fn.stdpath('data') ..'/site/pack/packer/opt'),
+      vim.fn.resolve(vim.fn.stdpath('data').. '/site/pack/packer/start'),
+      vim.fn.resolve(vim.fn.stdpath('data').. '/site/pack/packer-lib/opt'),
+    }
   })
 end
 
