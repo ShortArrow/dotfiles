a4fbc4fc02e21d4815da1a885dadddbadc808c5b
ShortArrow <bamboogeneral@shortarrow.jp>
2023-01-06 23:58:08 +0900

    fix cmp

--- nvim/lua/config/_cmp.lua
+++ nvim/lua/config/_cmp.lua
@@ -19,6 +19,11 @@ M.setup = function()
     vsnip = "[VSnip]",
     path = "[Path]",
   }
+  local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+  end
+
   local cmp = require('cmp')
   if cmp ~= nil then
     cmp.setup({
@@ -66,6 +71,12 @@ M.setup = function()
           function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
             else
               fallback()
             end
@@ -76,6 +87,8 @@ M.setup = function()
           function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
             else
               fallback()
             end
