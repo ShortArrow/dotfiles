7872ff3a5ac4fa8d406c40f93b42b50003913e89
ShortArrow <bamboogeneral@shortarrow.jp>
2023-01-08 23:21:48 +0900

    neotest

--- nvim/lua/my/lang.lua
+++ nvim/lua/my/lang.lua
@@ -6,5 +6,6 @@ M.lua = require('my.lang.lua')
 M.pwsh = require('my.lang.pwsh')
 M.deno = require('my.lang.deno')
 M.ts = require('my.lang.ts')
+M.rust = require('my.lang.rust')
 
 return M
---
+++ nvim/lua/my/lang/rust.lua
@@ -0,0 +1,7 @@
+local M   = {}
+
+M.neotest = {
+  args = { "--no-capture" },
+}
+
+return M
