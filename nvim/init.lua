dfe278d152849ae02f76b0fc5e4fb130dc5a507a
ShortArrow <bamboogeneral@shortarrow.jp>
2023-01-26 08:42:55 +0900

    severity signs for diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -83,4 +83,9 @@ else
       end,
     },
   })
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
 end
