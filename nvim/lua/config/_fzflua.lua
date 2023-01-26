dcd2f8c530663cff7a9053e6102f1d7252fd6eae
ShortArrow <bamboogeneral@shortarrow.jp>
2023-01-26 09:17:34 +0900

    fix luacheck

--- nvim/lua/config/_fzflua.lua
+++ nvim/lua/config/_fzflua.lua
@@ -3,6 +3,10 @@ local M = {}
 M.setup = function()
   require("fzf-lua").setup {
     file_icon_padding = ' ',
+    lsp = {
+      -- make lsp requests synchronous so they work with null-ls
+      async_or_timeout = 3000,
+    },
   }
 end
 
