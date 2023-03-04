483885a30a926ead8fd4f6bc2b0eda0c552b2f80
ShortArrow <bamboogeneral@shortarrow.jp>
2023-03-04 17:33:15 +0900

    fix toggleterm

--- nvim/lua/config/_toggleterm.lua
+++ nvim/lua/config/_toggleterm.lua
@@ -1,25 +1,19 @@
 local M = {}
 
 M.setup = function()
-  require('toggleterm').setup()
-end
+  local toggleterm = require("toggleterm")
+  toggleterm.setup()
 
--- #region slime
-M.set_slime_id = function()
-  vim.g.slime_get_jobid = vim.cmd [[echo &channel]]
-end
+  M.set_slime_id = function()
+    vim.g.slime_get_jobid = vim.cmd([[echo &channel]])
+  end
 
-local ok, plugin = pcall(require, 'toggleterm.terminal')
-if not ok then
-  print('not installed:', plugin)
-else
-  local Terminal     = plugin.Terminal
-  local repl_term    = Terminal:new({ on_open = M.set_slime_id })
+  local Terminal = plugin.Terminal
+  local repl_term = Terminal:new({ on_open = M.set_slime_id })
   M.toggle_repl_term = function()
     repl_term:toggle()
   end
-end
 
--- #endregion slime
+end
 
 return M
