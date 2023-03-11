474d82274ce1171341b4e10428130d6e4d97fb96
ShortArrow <bamboogeneral@shortarrow.jp>
2023-03-11 16:00:53 +0900

    vscode ui mode color

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,79 @@
 if vim.g.vscode then
   -- vscode extension
-  print('load vscode extension config')
-elseif 0 ~= vim.fn.exists('g:started_by_firenvim') then
+  print("load vscode extension config")
+  -- THEME CHANGER
+
+  function SetCursorLineNrColorInsert(mode)
+    -- Insert mode: blue
+    if mode == "i" then
+      vim.api.nvim_call_function("VSCodeNotify", { "nvim-theme.insert" })
+      -- Replace mode: red
+    elseif mode == "r" then
+      vim.api.nvim_call_function("VSCodeNotify", { "nvim-theme.replace" })
+    end
+  end
+
+  function SetCursorLineNrColorVisual()
+    vim.api.nvim_command("set updatetime=0")
+    vim.api.nvim_call_function("VSCodeNotify", { "nvim-theme.visual" })
+  end
+
+  vim.api.nvim_set_keymap(
+    "v",
+    "<SID>SetCursorLineNrColorVisual",
+    "<expr> v:lua.SetCursorLineNrColorVisual()",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "v<SID>SetCursorLineNrColorVisual",
+    "<script> v<SID>SetCursorLineNrColorVisual()",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "V<SID>SetCursorLineNrColorVisual",
+    "<script> V<SID>SetCursorLineNrColorVisual()",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-v><SID>SetCursorLineNrColorVisual",
+    "<script> <C-v><SID>SetCursorLineNrColorVisual()",
+    { silent = true }
+  )
+
+  vim.api.nvim_exec(
+    [[
+augroup CursorLineNrColorSwap
+autocmd!
+autocmd InsertEnter * call v:lua.SetCursorLineNrColorInsert(vim.fn.expand('<a:insertmode>'))
+autocmd InsertLeave * call vim.api.nvim_call_function('VSCodeNotify', {'nvim-theme.normal'})
+autocmd CursorHold * call vim.api.nvim_call_function('VSCodeNotify', {'nvim-theme.normal'})
+augroup END
+]],
+    false
+  )
+elseif 0 ~= vim.fn.exists("g:started_by_firenvim") then
   -- general config for firenvim
-  print('load firenvim config')
+  print("load firenvim config")
   vim.g.firenvim_config = {
     globalSettigs = {
-      alt = 'all',
+      alt = "all",
     },
     localSettings = {
-      [".*"] = {
-        cmdline = 'neovim',
-        content = 'text',
+          [".*"] = {
+        cmdline = "neovim",
+        content = "text",
         priority = 0,
-        selector = 'textarea',
-        takeover = 'never',
+        selector = "textarea",
+        takeover = "never",
       },
-    }
+    },
   }
 
   local setup_firenvim = function()
-    vim.bo.filetype = 'markdown'
+    vim.bo.filetype = "markdown"
     vim.wo.number = false
     vim.go.laststatus = 0
     vim.go.showtabline = 0
@@ -33,7 +86,7 @@ elseif 0 ~= vim.fn.exists('g:started_by_firenvim') then
     callback = setup_firenvim,
   })
 
-  local options = require('my.options')
+  local options = require("my.options")
   options.activate()
 
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -55,7 +108,7 @@ elseif 0 ~= vim.fn.exists('g:started_by_firenvim') then
 else
   -- ordinary neovim
 
-  local options = require('my.options')
+  local options = require("my.options")
   options.activate()
 
   local api = require("my")
