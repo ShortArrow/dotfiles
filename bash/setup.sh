bfbf1770eb871e2b2111c657c6b2b47bb2351c95
ShortArrow <bamboogeneral@shortarrow.jp>
2023-03-15 23:11:10 +0900

    fix bash setup

--- bash/setup.sh
+++ bash/setup.sh
@@ -5,11 +5,12 @@ rm -rf ~/.bash_myplug
 rm -rf ~/.bash_mycompletion.sh
 
 # caution! Don't needs slash at last of directory name.
-ln -s "$HOME/Documents/GitHub/dotfiles/bash/bash_myplug.sh" ~/.bash_myplug
-ln -s "$HOME/Documents/GitHub/dotfiles/bash/bash_mycompletion.sh" ~/.bash_mycompletion.sh
+REPO="$HOME/Documents/GitHub/dotfiles/bash"
+ln -s "$REPO/bash_myplug.sh" ~/.bash_myplug
+ln -s "$REPO/bash_mycompletion.sh" ~/.bash_mycompletion.sh
 
 # check link
-file ~/.bash_myplug # check link
+file ~/.bash_myplug          # check link
 file ~/.bash_mycompletion.sh # check link
 
 # display guides after run this script
