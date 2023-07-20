0c33f4f8d8cc9a732b9102f01810f761eb702bab
shortarrow <bamboogeneral@shortarrow.jp>
2023-07-20 15:43:54 +0900

    fix devcontainer cong

--- nvim/lua/config/_devcontainer.lua
+++ nvim/lua/config/_devcontainer.lua
@@ -1,7 +1,24 @@
 local M = {}
 
 M.setup = function()
-  require('devcontainer').setup {}
+  require("devcontainer").setup({
+    attach_mounts = {
+      always = true,
+      neovim_config = {
+        enabled = true,
+        options = { "readonly" },
+      },
+      neovim_data = {
+        enabled = false,
+        options = {},
+      },
+      -- Only useful if using neovim 0.8.0+
+      neovim_state = {
+        enabled = false,
+        options = {},
+      },
+    },
+  })
 end
 
 return M
