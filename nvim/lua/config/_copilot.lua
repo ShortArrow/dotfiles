4e967f07c851df40c276905f8ab20cc0a2695723
ShortArrow <bamboogeneral@shortarrow.jp>
2023-11-20 22:21:18 +0900

    update copilot keymap

--- nvim/lua/config/_copilot.lua
+++ nvim/lua/config/_copilot.lua
@@ -3,6 +3,9 @@ local M = {}
 M.setup = function()
   vim.g.copilot_no_tab_map = true
   vim.api.nvim_set_keymap("i", "<C-o>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-dismiss)", { silent = true })
+  vim.api.nvim_set_keymap("i", "<M-]>", "<Plug>(copilot-next)", { silent = true })
+  vim.api.nvim_set_keymap("i", "<M-[>", "<Plug>(copilot-previous)", { silent = true })
 end
 
 return M
