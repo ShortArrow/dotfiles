bea44f5769c3c630ddf687bf74aac65891c930d6
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-22 13:28:24 +0900

    add copilot chat

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -31,6 +31,19 @@ M.ordinalnvim = {
       require("copilot_cmp").setup()
     end
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   -- {
   --   "github/copilot.vim",
   --   config = get_config("_copilot").setup,
