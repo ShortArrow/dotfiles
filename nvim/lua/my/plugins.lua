73da2818b392ffb8df46ee90240bfad47ce3e869
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-23 10:30:16 +0900

    add copilot chat opts file

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -38,10 +38,7 @@ M.ordinalnvim = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
     },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
+    opts = get_config("_copilot_chat").opts,
     -- See Commands section for default commands if you want to lazy load on them
   },
   -- {
