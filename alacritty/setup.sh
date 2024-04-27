8fa8bb8cd8c48645cfc5754227683680010f673a
ShortArrow <bamboogeneral@shortarrow.jp>
2024-04-27 13:09:14 +0900

    update alacritty

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mkdir -p "$HOME/.config/alacritty/"
-rm -f "$HOME/.config/alacritty/alacritty.yml"
-ln -s "$HOME/Documents/GitHub/dotfiles/alacritty/alacritty.yml" "$HOME/.config/alacritty/"
+rm -rf "$HOME/.config/alacritty"
+ln -s "$HOME/Documents/GitHub/dotfiles/alacritty" "$HOME/.config/alacritty"
