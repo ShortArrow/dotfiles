3c46817ae5018e72212b6cb6657fa0d2616102a0
ShortArrow <bamboogeneral@shortarrow.jp>
2024-05-15 15:04:28 +0900

    add dotnet test

--- nvim/lua/my/lang.lua
+++ nvim/lua/my/lang.lua
@@ -10,5 +10,6 @@ M.ts = require('my.lang.ts')
 M.rust = require('my.lang.rust')
 M.clang = require('my.lang.clang')
 M.astro = require('my.lang.astro')
+M.csharp = require('my.lang.csharp')
 
 return M
