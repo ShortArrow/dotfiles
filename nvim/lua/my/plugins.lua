cf1141f19b2a9e8fbacbc99e9084cb25de48df02
ShortArrow <bamboogeneral@shortarrow.jp>
2024-07-28 21:42:56 +0900

    add markdown.nvim

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -25,6 +25,15 @@ M.ordinalnvim = {
     dev = false,
   },
   {
+    'MeanderingProgrammer/markdown.nvim',
+    main = "render-markdown",
+    opts = {},
+    name = 'render-markdown',                                                      -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
+  {
     "xiyaowong/transparent.nvim",
   },
   {
