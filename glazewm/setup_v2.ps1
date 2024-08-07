198918538054db3481af1857b426f5e4612a21a6
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-07 13:41:46 +0900

    update glazewm setup script v2 and v3 swap

--- glazewm/setup_v2.ps1
+++ glazewm/setup_v2.ps1
@@ -1,6 +1,6 @@
 $list = @("config.yaml")
 foreach ($item in $list) {
-  Remove-Item "$env:USERPROFILE/.glzr/glazewm/$item" -Force
-  New-Item -Type SymbolicLink -Path "$env:USERPROFILE/.glzr/glazewm/" -Name $item -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/glazewm/$item"
+  Remove-Item "$env:USERPROFILE/.glaze-wm/$item" -Force
+  New-Item -Type SymbolicLink -Path "$env:USERPROFILE/.glaze-wm/" -Name $item -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/glazewm/$item"
 }
 
