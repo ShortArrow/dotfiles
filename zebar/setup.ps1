4786099f25a4211c95c912cfefa5ab1b9967a907
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-07 13:51:29 +0900

    update zebar startup script path

--- zebar/setup.ps1
+++ zebar/setup.ps1
@@ -1,4 +1,4 @@
-$list = @("config.yaml", "script.js")
+$list = @("config.yaml", "script.js", "start.bat", "start.sh")
 foreach ($item in $list) {
   Remove-Item "$env:USERPROFILE/.glzr/zebar/$item" -Force
   New-Item -Type SymbolicLink -Path "$env:USERPROFILE/.glzr/zebar/" -Name $item -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/zebar/$item"
