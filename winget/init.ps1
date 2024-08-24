de08c1694b962a3591ccabc7e1523ba5968bfcf0
ShortArrow <bamboogeneral@shortarrow.jp>
2024-08-25 00:08:57 +0900

    add script for winget

---
+++ winget/init.ps1
@@ -0,0 +1,5 @@
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.PowerToys
+winget install -e --id 7zip.7zip
+winget install -e --id Git.Git
