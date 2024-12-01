2a7f04af1df4e10a551db4babcbe216fe7214154
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-02 00:43:32 +0900

    update vim setup script in pwsh

--- vim/setup.ps1
+++ vim/setup.ps1
@@ -1,8 +1,11 @@
 #! pwsh
 
+# Get the directory of the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 Remove-Item -Force $env:USERPROFILE/.vimrc
 
 New-Item -Type SymbolicLink `
   -Path $env:USERPROFILE `
   -Name ".vimrc" `
-  -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/vim/.vimrc"
+  -Value "$scriptDirectory/.vimrc"
