f972a91d0a52dda587f77f718fe49dc3f4401b31
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-05 09:15:20 +0900

    add setup script for wezterm in windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night'
 
 -- and finally, return the configuration to wezterm
 return config
