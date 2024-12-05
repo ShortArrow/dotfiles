50264211ebed0abbaf226c19b6be6a4551be66ed
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-05 09:42:52 +0900

    change wezterm color to tomorrow night

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,7 +12,7 @@ config.font = wezterm.font 'JetBrainsMonoNL Nerd Font'
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night Night'
 config.color_scheme = 'Tomorrow Night Night'
-config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'AdventureTime'
 
 -- and finally, return the configuration to wezterm
 return config
