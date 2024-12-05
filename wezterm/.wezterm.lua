b992e9fa3155f22ab755a3347a04e82ff0634501
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-05 09:23:52 +0900

    update color of wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Tokyo Night Night'
+config.color_scheme = 'Tomorrow Night Night'
+config.color_scheme = 'AdventureTime'
 
 -- and finally, return the configuration to wezterm
 return config
