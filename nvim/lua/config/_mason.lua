78131e82aef28d4840a427bf8e4be5ccd3a02409
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-06 11:44:43 +0900

    add lemminx for nvim

--- nvim/lua/config/_mason.lua
+++ nvim/lua/config/_mason.lua
@@ -115,6 +115,8 @@ M.setup = function()
         -- }
       elseif server_name == "intelephense" then
         _opts.settings = _api.lang.php.intelephense
+      elseif server_name == "lemminx" then
+        _opts.settings = _api.lang.xml.lemminx
       elseif server_name == "pyright" then
         _opts.settings = _api.lang.python.pyright
       elseif server_name == "pyls" then
