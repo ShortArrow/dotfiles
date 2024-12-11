01eaf0daa623f003dbfa7736a6c93fb4263b0399
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-11 11:38:46 +0900

    update admin check for schedule setter for glazewm

--- glazewm/schedule.ps1
+++ glazewm/schedule.ps1
@@ -1,10 +1,35 @@
 #! pwsh
 
+function IsAdmin() {
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+    $isAdmin = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+    return $isAdmin
+}
+
+function RestartWithAdmin()
+{
+    param(
+        [string]$scriptPath
+    )
+    $scriptPath = Resolve-Path $scriptPath
+    # if you want debug, add '-noe' option to $arguments
+    $arguments = @("-nop", "-nol", "-ex", "RemoteSigned", "-f", "$scriptPath")
+    Start-Process pwsh -ArgumentList $arguments -Verb RunAs -Wait -PassThru
+    exit
+}
+
+if (-not (IsAdmin)) {
+    $scriptPath = $MyInvocation.MyCommand.Path
+    RestartWithAdmin($scriptPath)
+}
+
 $taskName = "GlazeWM_Task"
 $glazewmPath = Resolve-Path `
     -Path "$env:USERPROFILE/Documents/GitHub/glazewm/target/release/glazewm.exe"
 $argument = @(
         "-c `"`$PSStyle.OutputRendering='Ansi'",
+        "`$host.UI.RawUI.WindowTitle = 'GlazewmLog'",
         "Start-Process -NoNewWindow '$glazewmPath' -ArgumentList 'start'",
         "pause",
         "`""
@@ -25,4 +50,6 @@ Register-ScheduledTask `
     -Action $action `
     -Settings $settings `
     -Force
+
+exit
 
