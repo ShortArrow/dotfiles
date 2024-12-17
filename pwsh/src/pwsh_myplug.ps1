f89f6e347c510dd8005a3d831c26d17b6eb95ddc
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-17 15:13:30 +0900

    add gitignore generator for pwsh

--- pwsh/src/pwsh_myplug.ps1
+++ pwsh/src/pwsh_myplug.ps1
@@ -295,3 +295,13 @@ function SetDefaultApp()
 }
 New-Alias -Name Set-DefaultApp -Value SetDefaultApp -Force
 
+#For PowerShell v3
+Function gig {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$list
+  )
+  $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
+  Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | select -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii
+}
+
