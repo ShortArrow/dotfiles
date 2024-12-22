32811108f7d5e63f37bc34c3aaeb01e3009a3181
ShortArrow <bamboogeneral@shortarrow.jp>
2024-12-23 07:02:15 +0900

    update wezterm config hide tab if only one tab true

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,5 +14,7 @@ config.color_scheme = 'Tokyo Night Night'
 config.color_scheme = 'Tomorrow Night Night'
 -- config.color_scheme = 'AdventureTime'
 
+config.hide_tab_bar_if_only_one_tab = true
+
 -- and finally, return the configuration to wezterm
 return config
