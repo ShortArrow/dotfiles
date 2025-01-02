267611d06ba5eec2c5462d420db6ede3118dd247
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-03 08:00:03 +0900

    add vscode setup script for linux

---
+++ code/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir=$(dirname "$(readlink -f "$0")")
+config_dir="$HOME/.config/Code/User"
+
+rm -f "$config_dir/keybindings.json"
+rm -f "$config_dir/settings.json"
+
+ln -s "$script_dir/keybindings.json" "$config_dir/keybindings.json"
+ln -s "$script_dir/settings.json" "$config_dir/settings.json"
+
+file "$config_dir/keybindings.json"
+file "$config_dir/settings.json"
