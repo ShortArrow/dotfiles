5ed4607f9fa1b9444ba67e883ea0659c9f6db402
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-04 07:56:16 +0900

    update window size of snacks.nvim news

--- nvim/lua/my/keymaps.lua
+++ nvim/lua/my/keymaps.lua
@@ -166,8 +166,8 @@ M.maps = {
       function()
         Snacks.win({
           file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
+          width = 0.9,
+          height = 0.9,
           wo = {
             spell = false,
             wrap = false,
