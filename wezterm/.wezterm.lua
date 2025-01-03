8f7ac2ee8360e03df545c970e383fddfc3ca4bc6
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-04 03:35:33 +0900

    update background color in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,6 +21,21 @@ config.color_scheme = 'Kibble'
 config.hide_tab_bar_if_only_one_tab = true
 
 config.enable_wayland = false
+config.window_background_gradient =
+{
+  -- orientation = 'Vertical',
+  -- preset = 'Cool',
+  colors = {
+    "#000010",
+    "#001020",
+    "#000010",
+  },
+  blend = 'Hsv',
+  noise = 200,
+  orientation = { Linear = { angle = -45.0 } },
+}
+
+config.window_background_opacity = 0.9
 
 -- and finally, return the configuration to wezterm
 return config
