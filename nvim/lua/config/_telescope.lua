12823f88ef2a109edf8c874630159084d27319da
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-07 04:44:53 +0900

    update ripgrep telescope config for hidden file

--- nvim/lua/config/_telescope.lua
+++ nvim/lua/config/_telescope.lua
@@ -52,6 +52,26 @@ M.setup = function()
           width = 0.6,
         },
       },
+      -- configure to use ripgrep
+      vimgrep_arguments = {
+        "rg",
+        "--follow",        -- Follow symbolic links
+        "--hidden",        -- Search for hidden files
+        "--no-heading",    -- Don't group matches by each file
+        "--with-filename", -- Print the file path with the matched lines
+        "--line-number",   -- Show line numbers
+        "--column",        -- Show column numbers
+        "--smart-case",    -- Smart case search
+
+        -- Exclude some patterns from search
+        "--glob=!**/.git/*",
+        "--glob=!**/.idea/*",
+        "--glob=!**/.vscode/*",
+        "--glob=!**/build/*",
+        "--glob=!**/dist/*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
     },
     pickers = {
       registers = {
@@ -59,6 +79,23 @@ M.setup = function()
       },
       lsp_code_actions = {
         theme = "cursor",
+      },
+      find_files = {
+        hidden = true,
+        -- needed to exclude some files & dirs from general search
+        -- when not included or specified in .gitignore
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+        },
       },
     },
     extensions = {},
