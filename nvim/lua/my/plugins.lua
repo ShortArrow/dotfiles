dbc8a1718a7f40a100690e8e0b010ce4bc6a534a
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-20 20:10:09 +0900

    add lazydev.nvim

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -423,6 +423,19 @@ M.ordinalnvim = {
   -- # Lua
   -- ################################################
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim",        words = { "Snacks" } },
+        { path = "lazy.nvim",          words = { "LazyVim" } },
+      },
+    },
+  },
+  {
     "folke/neodev.nvim",
     -- dependencies = {
     --   "williamboman/mason.nvim",
