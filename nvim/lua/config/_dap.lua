9961272b12033955fff1d9539d0bfb1bc1f03a3c
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-28 07:56:50 +0900

    add dap config

---
+++ nvim/lua/config/_dap.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+M.enableHooks = function()
+  local dap = require("dap")
+  local dapui = require("dapui")
+  dap.listeners.before.attach.dapui_config = function()
+    dapui.open()
+  end
+  dap.listeners.before.launch.dapui_config = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited.dapui_config = function()
+    dapui.close()
+  end
+end
+
+return M
--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -432,6 +432,7 @@ M.ordinalnvim = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         { path = "snacks.nvim",        words = { "Snacks" } },
         { path = "lazy.nvim",          words = { "LazyVim" } },
+        "nvim-dap-ui",
       },
     },
   },
