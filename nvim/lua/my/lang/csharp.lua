cf3ede0e9bc2aa9aac27212da64a277b87620002
ShortArrow <bamboogeneral@shortarrow.jp>
2025-01-28 12:51:35 +0900

    update neotest dotnet for nvim

--- nvim/lua/my/lang/csharp.lua
+++ nvim/lua/my/lang/csharp.lua
@@ -6,7 +6,7 @@ M.neotest = {
     -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
     args = { justMyCode = false },
     -- Enter the name of your dap adapter, the default value is netcoredbg
-    adapter_name = "netcoredbg"
+    adapter_name = "coreclr"
   },
   -- Let the test-discovery know about your custom attributes (otherwise tests will not be picked up)
   -- Note: Only custom attributes for non-parameterized tests should be added here. See the support note about parameterized tests
