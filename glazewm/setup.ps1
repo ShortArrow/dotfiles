4721d6fc2c300bfb2be3f93bd2aef962111a30ae
ShortArrow <bamboogeneral@shortarrow.jp>
2025-02-12 11:23:30 +0900

    update glazewm setup script

--- glazewm/setup.ps1
+++ glazewm/setup.ps1
@@ -1,6 +1,14 @@
+#! pwsh
+ 
+# Get the directory of the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 $list = @("config.yaml")
 foreach ($item in $list) {
-  Remove-Item "$env:USERPROFILE/.glzr/glazewm/$item" -Force
-  New-Item -Type SymbolicLink -Path "$env:USERPROFILE/.glzr/glazewm/" -Name $item -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/glazewm/$item"
+  Remove-Item -Force "$env:USERPROFILE/.glzr/glazewm/$item"
+  New-Item `
+    -Type SymbolicLink `
+    -Path "$env:USERPROFILE/.glzr/glazewm/" `
+    -Name $item `
+    -Value "$scriptDirectory/$item"
 }
-
