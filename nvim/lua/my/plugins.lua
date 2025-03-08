af7b534c8c56fba77212769dc256c66d7e46f4dc
ShortArrow <bamboogeneral@shortarrow.jp>
2025-03-08 09:26:29 +0900

    add oil.nvim to neovim config

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -161,6 +161,17 @@ M.ordinalnvim = {
   -- ################################################
   -- # Explorer
   -- ################################################
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
   { "obaland/vfiler-column-devicons" },
   {
     "obaland/vfiler.vim",
