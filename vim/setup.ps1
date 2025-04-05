d2595aa86df52473e17b61a7443b1a7126bae40b
ShortArrow <bamboogeneral@shortarrow.jp>
2025-04-05 17:43:19 +0900

    add gvim config

--- vim/setup.ps1
+++ vim/setup.ps1
@@ -3,9 +3,19 @@
 # Get the directory of the script
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-Remove-Item -Force $env:USERPROFILE/.vimrc
+$fileName1 = ".vimrc"
+$fileName2 = ".gvimrc"
+
+Remove-Item -Force $env:USERPROFILE/$fileName1
+Remove-Item -Force $env:USERPROFILE/$fileName2
 
 New-Item -Type SymbolicLink `
   -Path $env:USERPROFILE `
-  -Name ".vimrc" `
-  -Value "$scriptDirectory/.vimrc"
+  -Name $fileName1 `
+  -Value $scriptDirectory/$fileName1
+
+New-Item -Type SymbolicLink `
+  -Path $env:USERPROFILE `
+  -Name $fileName2 `
+  -Value $scriptDirectory/$fileName2
+
