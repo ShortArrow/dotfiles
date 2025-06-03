f0db08f93b15b446b988f81abbef8ce706088bdc
ShortArrow <bamboogeneral@shortarrow.jp>
2025-06-03 17:45:54 +0900

    Update starship setup script to create config directory if it does not exist

--- starship/setup.ps1
+++ starship/setup.ps1
@@ -8,6 +8,11 @@ $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # Set the target directory
 $targetDirectory = "$env:USERPROFILE/.config/"
 
+# Make directory if it does not exist
+if (-not (Test-Path -Path $targetDirectory)) {
+  New-Item -ItemType Directory -Path $targetDirectory
+}
+
 $list = @("starship.toml")
 foreach ($item in $list) {
   Remove-Item "$targetDirectory/$item" -Force
