65108ec07ab5c706153ec602af41b1ab59e09e56
ShortArrow <bamboogeneral@shortarrow.jp>
2025-06-04 14:13:50 +0900

    Update the PowerShell setup script to create a symbolic link to the source directory

--- pwsh/setup.ps1
+++ pwsh/setup.ps1
@@ -1,12 +1,20 @@
-$ProfileDir = (Split-Path $PROFILE -Parent)
+#!pwsh
 
-if (Test-Path "$ProfileDir/myplug") {
-  Remove-Item "$ProfileDir/myplug" -Force
+# Get the directory of the script
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+echo "Script directory: $scriptDirectory"
+
+# Set the target directory
+$targetDirectory = (Split-Path $PROFILE -Parent)
+echo "Target directory: $targetDirectory"
+
+if (Test-Path "$targetDirectory/myplug") {
+    Remove-Item "$targetDirectory/myplug" -Force
 }
-New-Item -Type SymbolicLink -Path "$ProfileDir" -Name "myplug" -Value "$env:USERPROFILE/Documents/GitHub/dotfiles/pwsh/src"
+New-Item -Type SymbolicLink -Path "$targetDirectory" -Name "myplug" -Value "$scriptDirectory/src"
 
 Get-ChildItem -Path $env:LOCALAPPDATA -Force -ErrorAction 'silentlycontinue' |
 Where-Object { $_.Attributes -match "ReparsePoint" -and $_.Name -match "pwsh_myplug" }
 
-Write-Output ". $(Split-Path $PROFILE -Parent)/myplug/pwsh_myplug.ps1" |
+Write-Output ". $targetDirectory/myplug/pwsh_myplug.ps1" |
 Out-File -FilePath $PROFILE -Encoding utf8 -Force
