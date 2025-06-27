ff397195498c3cbc8a90e2f6949dea11bf25905d
ShortArrow <bamboogeneral@shortarrow.jp>
2025-06-27 16:30:21 +0900

    Fix: update lazygit config file extension from .yaml to .yml

--- lazygit/setup.ps1
+++ lazygit/setup.ps1
@@ -2,12 +2,14 @@
 
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-$targetDirectory = "$env:APPDATA"
+$targetDirectory = "$env:LOCALAPPDATA"
+
+# $env:LOCALAPPDATA\lazygit\config.yml
 
 $item = "lazygit"
 
 if (Test-Path "$targetDirectory/$item") {
-    Remove-Item "$targetDirectory/$item" -Force
+    Remove-Item "$targetDirectory/$item" -Force -Recurse
 }
 
 New-Item `
