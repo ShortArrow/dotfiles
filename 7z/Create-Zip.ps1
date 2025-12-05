fceda0a07bca8b05b6ca5b97a6e412583c37e581
ShortArrow <bamboogeneral@shortarrow.jp>
2025-12-05 16:46:32 +0900

    add: shiftjis encoded zip file creation script with 7z in pwsh

---
+++ 7z/Create-Zip.ps1
@@ -0,0 +1,24 @@
+param(
+  [string]
+  $Destination=".",
+  [string]
+  $Source
+)
+
+if(!(Test-Path $Source))
+{
+  Write-Host "Unexists path" -ForegroundColor Red
+}
+
+$7zpass = (New-Password)
+
+Write-Host $7zpass
+
+$ArchiveName = "$(Get-Date -Format 'yyMMddHHmmss').zip"
+
+$Destination = (Join-Path -Path $Destination -ChildPath $ArchiveName)
+
+Write-Host "Source: ${Source}"
+Write-Host "Destination: ${Destination}"
+
+7z a -tzip -mcp=932 -p"$7zpass" $Destination $Source
