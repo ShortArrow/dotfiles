4c1ba4bc92cf004a299c6637346841b05e30e2a9
ShortArrow <bamboogeneral@shortarrow.jp>
2025-12-24 11:20:24 +0900

    update: Update PATH management scripts and files for Windows

--- windows/ApplyPath.ps1
+++ windows/ApplyPath.ps1
@@ -1,26 +1,50 @@
 #!pwsh
-$Raw = Get-Content -Path "$PSScriptRoot/PATH.txt"
+param(
+  [ValidateSet("User", "Machine")]
+  [string]$Target = "User"
+)
+
+[bool]$IsUser = $Target -eq "User"
+[string]$Name = "Path"
+
+[string]$Source = $IsUser ? "$PSScriptRoot/PATH.txt" : "$PSScriptRoot/SYSTEM_PATH.txt"
+$Raw = Get-Content -Path $Source
 $ConcatedPath = $Raw -join ";"
-$PathRegistryLocation = "HKCU:\Environment"
+
+[string]$UserPath = "HKCU:\Environment"
+[string]$SystemPath = "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment"
+[string]$PathRegistryLocation = $IsUser ? $UserPath : $SystemPath
 
 Write-Host -ForegroundColor Yellow "Current PATH environment variable:"
-$CurrentPathEntry = Get-ItemProperty -Path $PathRegistryLocation -Name Path
+$CurrentPathEntry = Get-ItemProperty -Path $PathRegistryLocation -Name $Name
 $CurrentPathEntry
 
 Write-Host -ForegroundColor Red "WARNING: This script will overwrite your current PATH environment variable!"
 
 $Response = Read-Host "The following PATH will be applied (y/N)"
 
-if ($Response -eq "y")
-{
-  $BackupPath = Join-Path $PSScriptRoot "PATH.bk.txt"
+function Get-BackupName {
+  param (
+    [string]$FilePath
+  )
+  $FullPath = Get-Item -Path $FilePath
+  $Directory = $FullPath.DirectoryName
+  $BaseName = $FullPath.BaseName
+  $Extension = $FullPath.Extension
+  $NewPath = Join-Path -Path $Directory -ChildPath ("$BaseName.bk$Extension")
+  return $NewPath
+}
+
+if ($Response -eq "y") {
+  
+  $BackupPath = Get-BackupName -FilePath $Source
   $CurrentPathEntry.Path -split ";" | Set-Content -Path $BackupPath
   Write-Host -ForegroundColor Cyan "Backed up current PATH to $BackupPath"
 
-  Set-ItemProperty -Path $PathRegistryLocation -Name Path -Value $ConcatedPath
+  Set-ItemProperty -Path $PathRegistryLocation -Name $Name -Value $ConcatedPath
   Write-Host -ForegroundColor Green "New PATH environment variable applied:"
-} else
-{
+}
+else {
   Write-Host -ForegroundColor Cyan "Operation cancelled by user. No changes made."
   exit
 }
--- windows/doctor.ps1
+++ windows/doctor.ps1
@@ -1,1 +1,3 @@
-Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled
+Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name LongPathsEnabled
+Get-ItemProperty -Path "HKCU:\Environment" -Name Path
+Get-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment" -Name Path
