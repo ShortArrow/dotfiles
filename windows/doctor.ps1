4ca4449b9b75b10c9c7179cb4e5994e51107cbf9
ShortArrow <bamboogeneral@shortarrow.jp>
2026-01-06 09:15:42 +0900

    update: windows/doctor.ps1 to add code page checks

--- windows/doctor.ps1
+++ windows/doctor.ps1
@@ -1,3 +1,48 @@
-Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name LongPathsEnabled
-Get-ItemProperty -Path "HKCU:\Environment" -Name Path
-Get-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment" -Name Path
+function Show-CheckResult()
+{
+  param (
+    [string]$TargetPath,
+    [string]$TargetName,
+    [string]$ExpectedValue,
+    [string]$ActualValue
+  )
+  Write-Host "- [" -NoNewline
+  $IsOK = $ExpectedValue -eq $ActualValue
+  Write-Host $($IsOK ? "OK" : "NG") -ForegroundColor $($IsOK ? "Green" : "Red") -NoNewline
+  Write-Host "]: " -NoNewline
+  Write-Host "$ActualValue" -NoNewline
+  if (-not $IsOK)
+  {
+    Write-Host ", Expected: $ExpectedValue" -ForegroundColor Yellow -NoNewline
+  }
+  Write-Host ", ($TargetPath\$TargetName)" -ForegroundColor DarkGray
+}
+
+function Test-ItemPropertyValue()
+{
+  param (
+    [string]$TargetPath,
+    [string]$TargetName,
+    [string]$ExpectedValue
+  )
+  $ActualValue = Get-ItemPropertyValue -Path "$TargetPath" -Name $TargetName
+  Show-CheckResult -TargetPath $TargetPath -TargetName $TargetName -ExpectedValue $ExpectedValue -ActualValue $ActualValue
+}
+
+# Only Show
+$UserEnvPath = "HKCU:\Environment"
+Write-Host "User Environment Variables:" -ForegroundColor Cyan
+Get-ItemPropertyValue -Path $UserEnvPath -Name Path
+$SystemEnvPath = "HKLM:\System\CurrentControlSet\Control\Session Manager\Environment"
+Write-Host "System Environment Variables:" -ForegroundColor Cyan
+Get-ItemPropertyValue -Path $SystemEnvPath -Name Path
+
+# Check diff
+Write-Host "Checking Windows Environment Variables..." -ForegroundColor Cyan
+$FileSystemPath = "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem"
+Test-ItemPropertyValue -TargetPath $FileSystemPath -TargetName "LongPathsEnabled" -ExpectedValue "1"
+$CodePagePath = "HKLM:\SYSTEM\CurrentControlSet\Control\Nls\CodePage"
+Test-ItemPropertyValue -TargetPath $CodePagePath -TargetName "ACP" -ExpectedValue "65001"
+Test-ItemPropertyValue -TargetPath $CodePagePath -TargetName "OEMCP" -ExpectedValue "65001"
+Test-ItemPropertyValue -TargetPath $CodePagePath -TargetName "MACCP" -ExpectedValue "65001"
+
