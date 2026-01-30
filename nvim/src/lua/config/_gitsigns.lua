67200382393cf5af58ad7f533459ed9caa45e0e2
ShortArrow <bamboogeneral@shortarrow.jp>
2026-01-30 17:18:47 +0900

    update: Add ignore_whitespace_change_at_eol to gitsigns config

--- nvim/src/lua/config/_gitsigns.lua
+++ nvim/src/lua/config/_gitsigns.lua
@@ -5,6 +5,8 @@ M.setup = function()
     attach_to_untracked = false,
     diff_opts = {
       internal = true,
+      -- Ignore CRLF/LF-only differences while keeping other whitespace diffs
+      ignore_whitespace_change_at_eol = true,
     },
     signcolumn = true,
     numhl      = true,
