441d874008da3997540caf6d4e306c4d2ceb6ab5
ShortArrow <bamboogeneral@shortarrow.jp>
2026-03-20 05:10:14 +0900

    add: vale rule in nvim

--- nvim/src/lua/config/_lint.lua
+++ nvim/src/lua/config/_lint.lua
@@ -2,6 +2,11 @@ local M = {}
 
 M.setup = function()
   local lint = require('lint')
+  if vim.fn.executable("vale") == 0 then
+    lint.linters_by_ft.markdown = vim.tbl_filter(function(name)
+      return name ~= "vale"
+    end, lint.linters_by_ft.markdown or {})
+  end
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
       lint.try_lint()
