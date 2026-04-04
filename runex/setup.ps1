743d01bda9aebe8e606a977b497141c97d4a12d1
ShortArrow <bamboogeneral@shortarrow.jp>
2026-04-04 20:26:39 +0900

    update: runex/setup.ps1

--- runex/setup.ps1
+++ runex/setup.ps1
@@ -1,12 +1,16 @@
 #!pwsh
 
-# Target is "$APPDATA/runex/config.toml" (Windows) or "~/.config/runex/config.toml" (others)
+# Target is "~/.config/runex/config.toml" on all platforms (XDG_CONFIG_HOME or ~/.config)
 
 # Get the directory of the script
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # Set the target directory
-$targetDirectory = "$env:APPDATA/runex"
+$xdgConfigHome = $env:XDG_CONFIG_HOME
+if (-not $xdgConfigHome) {
+  $xdgConfigHome = "$env:USERPROFILE/.config"
+}
+$targetDirectory = "$xdgConfigHome/runex"
 
 # Make directory if it does not exist
 if (-not (Test-Path -Path $targetDirectory)) {
