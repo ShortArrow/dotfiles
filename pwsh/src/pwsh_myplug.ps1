dd59e0e0cee8f7fc021f60b8874575bfcf68704b
ShortArrow <bamboogeneral@shortarrow.jp>
2026-04-09 17:29:14 +0900

    update: gitignore generator in PowerShell

--- pwsh/src/pwsh_myplug.ps1
+++ pwsh/src/pwsh_myplug.ps1
@@ -412,7 +412,8 @@ Function gig {
     [string[]]$list
   )
   $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
-  Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | select -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii
+  $DebugPreference = 'SilentlyContinue'
+  Invoke-RestMethod -Uri "https://www.toptal.com/developers/gitignore/api/$params"
 }
 
 
